38cdabdda3455e5f691e4e2a17e5281afdb2acd5
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-09-26 14:21:03 +0100

    cinel-aula5-react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,6 +24,20 @@ export default function Header({logo, carrinhoCompras, addCarrinho}) {
           {sidebarAberta && (
             <div className="">
               <ProdutosCarrinho addCarrinho={addCarrinho} />
+              <div className="container-total">
+                <div className="container-itens">
+              <div className="subtotal-sidebar">
+                <p>Subtotal</p>
+                <p>0 €uros</p>
+              </div>
+              <hr/>
+              <div className="total-sidebar" >
+              <b>Total</b>
+              <b>0 €uros</b>
+              </div>
+              <button className="button-finalizar-pedido">Finalizar pedido</button>
+              </div>
+              </div>
         </div>
           )}
         </div>
